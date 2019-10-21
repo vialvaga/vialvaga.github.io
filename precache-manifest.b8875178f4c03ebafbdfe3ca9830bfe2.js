ff9dcaa4b60c99d352b1950248e43e770fb06378
vialvaga <vavg31@gmail.com>
2019-10-21 15:37:48 -0400

    Updates

---
+++ precache-manifest.b8875178f4c03ebafbdfe3ca9830bfe2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31446bd9bce98aead5270e5698d77cb8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "05970ef866dd66b52e49",
+    "url": "/monsters-rolodex/static/css/main.eb52eb65.chunk.css"
+  },
+  {
+    "revision": "3663d9b86bcebd379dd1",
+    "url": "/monsters-rolodex/static/js/2.87b9e15c.chunk.js"
+  },
+  {
+    "revision": "05970ef866dd66b52e49",
+    "url": "/monsters-rolodex/static/js/main.35671cd9.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
